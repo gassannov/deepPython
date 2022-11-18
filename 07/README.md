57606eacd9ced6ff17c420dacf2397ad69424656
Marat Gasanov <maratgasanov6@gmail.com>
2022-11-18 16:22:41 +0300

    add 07 dz readme

---
+++ 07/README.md
@@ -0,0 +1,4 @@
+![test_run](images/test_passed.png)
+![server_coverage](images/linters.png)
+![client_coverage](images/pmatrix_coverage.png)
+![server_linters](images/cffi_coverage.png)
--- 07/main.py
+++ 07/main.py
@@ -1,50 +1,49 @@
 import random
-import matrix_c
-import matrix_p
 import time
 import cutils
+import matrix_c
+import matrix_p
 
 
 def print_matrix(matrix):
-    for i in range(3):
-        for j in range(3):
-            print(matrix[i][j], end=' ')
+    for row in range(3):
+        for column in range(3):
+            print(matrix[row][column], end=' ')
         print()
 
 
 def make_random_matrix(num_rows, num_columns):
     matrix = [[0] * num_columns] * num_rows
-    for i in range(num_rows):
-        for j in range(num_columns):
-            matrix[i][j] = random.randint(random_low, random_high)
+    for row in range(num_rows):
+        for column in range(num_columns):
+            matrix[row][column] = random.randint(RANDOM_LOW, RANDOM_HIGH)
     return matrix
 
 
 if __name__ == '__main__':
-    num_rows, num_columns = 3, 3
-    random_low, random_high = 1, 4
-    matrix_number = 1000
+    NUM_ROWS, NUM_COLUMNS = 3, 3
+    RANDOM_LOW, RANDOM_HIGH = 1, 4
+    MATRIX_NUMBER = 1000
 
-    matrices = [make_random_matrix(num_rows, num_columns) for i in range(matrix_number)]
+    matrices = [make_random_matrix(NUM_ROWS, NUM_COLUMNS)
+                for i in range(MATRIX_NUMBER)]
     matrix_out = matrices[0]
     start_time = time.time()
-    for i in range(1, matrix_number):
+    for i in range(1, MATRIX_NUMBER):
         matrix_out = matrix_p.mul_matrix(matrix_out, matrices[i])
     end_time = time.time()
     print(f'in python time = {end_time - start_time}')
 
     matrix_out = matrices[0]
     start_time = time.time()
-    for i in range(1, matrix_number):
+    for i in range(1, MATRIX_NUMBER):
         matrix_out = matrix_c.mul_matrix(matrix_out, matrices[i])
     end_time = time.time()
     print(f'in cffi time = {end_time - start_time}')
 
     matrix_out = matrices[0]
     start_time = time.time()
-    for i in range(1, matrix_number):
+    for i in range(1, MATRIX_NUMBER):
         matrix_out = cutils.matmul(matrix_out, matrices[i])
     end_time = time.time()
     print(f'in capi time = {end_time - start_time}')
-
-
